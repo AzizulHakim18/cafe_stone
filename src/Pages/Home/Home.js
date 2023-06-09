68a33ec0e96611b29550dfffb717ae2181f18abf
AzizulHakim18 <hakimrony18@gamil.com>
2023-06-10 00:59:40 +0600

    search option added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Products from './Products';
+import Form from 'muicss/lib/react/form';
+import Input from 'muicss/lib/react/input';
+import Button from 'muicss/lib/react/button';
 
 const Home = () => {
-    return (
-        <div>
-            this is home page
-        </div>
-    );
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+
+    const [q, setQ] = useState("");
+    const [searchParam] = useState(["name"]);
+
+    useEffect(() => {
+        fetch("CafeData.json")
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
+    console.log(items);
+    const search = (item) => {
+        return items.filter((item) => {
+            return searchParam.some((newItem) => {
+                return (
+                    item[newItem]
+                        .toString()
+                        .toLowerCase()
+                        .indexOf(q.toLowerCase()) > -1
+                )
+            })
+        })
+    }
+
+    if (error) {
+        return <>{error.message}</>;
+    } else if (!isLoaded) {
+        return <>loading...</>;
+    } else {
+
+
+        return (
+            <div>
+
+                {/* search section */}
+                <Form inline={true}>
+                    <Input
+                        type='search'
+                        name='search-form'
+                        placeholder='Search .....'
+                        value={q}
+                        onChange={(e) => setQ(e.target.value)}
+                    />
+                </Form>
+
+                {/* all item get by map method  */}
+                <div className='hero_cart'>
+                    <ul>
+                        {search(items).map(item => <Products
+                            key={item.id}
+                            item={item}
+                        ></Products>)}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+
 };
 
 export default Home;
---
+++ src/Pages/Home/Products.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+const Products = ({ item }) => {
+    console.log(item);
+    const { image, category, id, name, prices, quantity, varients, description } = item;
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={image}
+                    alt=""
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Buy Now
+                </Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default Products;
